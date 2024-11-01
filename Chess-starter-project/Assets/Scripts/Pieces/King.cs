1cb1c0babc8065fe6816f339582bda1559534a2b
Diana Kuklina <kuklinadiana12@gmail.com>
2024-11-01 13:43:53 +0300

    end base chess

--- Chess-starter-project/Assets/Scripts/Pieces/King.cs
+++ Chess-starter-project/Assets/Scripts/Pieces/King.cs
@@ -36,6 +36,25 @@ public class King : Piece
     public override List<Vector2Int> MoveLocations(Vector2Int gridPoint)
     {
         List<Vector2Int> locations = new List<Vector2Int>();
+
+        List<Vector2Int> directions = new List<Vector2Int>(BishopDirections);
+        directions.AddRange(RookDirections);
+
+        foreach (Vector2Int dir in directions)
+        {
+            Vector2Int nextGridPoint = new Vector2Int(gridPoint.x + dir.x, gridPoint.y + dir.y);
+            locations.Add(nextGridPoint);
+        }
+
+        //locations.Add(new Vector2Int(gridPoint.x - 1, gridPoint.y));
+        //locations.Add(new Vector2Int(gridPoint.x + 1, gridPoint.y));
+        //locations.Add(new Vector2Int(gridPoint.x, gridPoint.y + 1));
+        //locations.Add(new Vector2Int(gridPoint.x, gridPoint.y - 1));
+        //locations.Add(new Vector2Int(gridPoint.x + 1, gridPoint.y - 1));
+        //locations.Add(new Vector2Int(gridPoint.x + 1, gridPoint.y + 1));
+        //locations.Add(new Vector2Int(gridPoint.x - 1, gridPoint.y - 1));
+        //locations.Add(new Vector2Int(gridPoint.x - 1, gridPoint.y + 1));
+
         return locations;
     }
 }
